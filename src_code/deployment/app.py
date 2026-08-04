dc84ec94f884c4549348a058405f3e4e6f0ddd26
naveen-wireless-ai-ml <nvnggrwl89@gmail.com>
2026-08-04 13:34:38 +0000

    Second commit

--- src_code/deployment/app.py
+++ src_code/deployment/app.py
@@ -17,13 +17,27 @@ This application predicts estimated product sales for a store.
 # User input widgets for each feature
 with st.form("Sales Estimation"):
     # Product Inputs with min/max constraints based on EDA
-    product_categories = ['Frozen Foods', 'Dairy', 'Canned',
-                        'Baking Goods', 'Health and Hygiene',
-                        'Snack Foods', 'Meat', 'Household',
-                        'Hard Drinks', 'Fruits and Vegetables',
-                        'Breads', 'Soft Drinks', 'Breakfast',
-                        'Others', 'Starchy Foods', 'Seafood'
-    ]
+    # Category to Prefix mapping
+    CATEGORY_PREFIX_MAP = {
+        'Hard Drinks': 'DR',
+        'Soft Drinks': 'DR',
+        'Frozen Foods': 'FD',
+        'Dairy': 'FD',
+        'Canned': 'FD',
+        'Baking Goods': 'FD',
+        'Snack Foods': 'FD',
+        'Meat': 'FD',
+        'Fruits and Vegetables': 'FD',
+        'Breads': 'FD',
+        'Breakfast': 'FD',
+        'Starchy Foods': 'FD',
+        'Seafood': 'FD',
+        'Health and Hygiene': 'NC',
+        'Household': 'NC',
+        'Others': 'NC'
+    }
+
+    product_categories = list(CATEGORY_PREFIX_MAP.keys())
 
     # Store Specification
     st.subheader("Enter Details Below for Product Sale Forecast")
@@ -40,11 +54,11 @@ with st.form("Sales Estimation"):
     st.markdown("#### 2. Product Details")
     col1, col2 = st.columns(2)
     with col1:
-        product_id = st.text_input("Product ID", value="FD306")
+        product_type = st.selectbox("Product Type", product_categories)
         product_mrp = st.number_input("Product MRP", min_value=5.0, max_value=500.0, value=117.08)
         product_weight = st.number_input("Product Weight", min_value=3.0, max_value=30.0, value=12.66)
     with col2:
-        product_type = st.selectbox("Product Type", product_categories)
+        product_id = st.text_input("Product ID", value="FD306")
         sugar_content = st.selectbox("Sugar Content", ["Low Sugar", "Regular", "No Sugar"])
         allocated_area = st.number_input("Allocated Area", min_value=0.0001, max_value=0.3, value=0.027, format="%.3f")
     submitted = st.form_submit_button("Predict Purchase")
